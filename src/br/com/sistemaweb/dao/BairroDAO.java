9b611611bb93fcfd4e80d0f714d7037f9422be21
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-06-15 11:46:10 -0300

    model / DAO de Bairro

---
+++ src/br/com/sistemaweb/dao/BairroDAO.java
@@ -0,0 +1,113 @@
+package br.com.sistemaweb.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.sistemaweb.factory.ConnectionFactory;
+import br.com.sistemaweb.javabean.model.Bairro;
+
+public class BairroDAO {
+
+	private Connection connection;
+	
+	public BairroDAO() {
+		this.connection = new ConnectionFactory().getConnection();
+	}
+	
+	public int totalDeRegistros(String pesquisa, String campoPesquisa) throws SQLException {
+		String sql = "SELECT count(*) AS contaRegistros FROM bairro WHERE " + campoPesquisa + " LIKE '%" + pesquisa + "%'";
+		int totalRegistros = 0;
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				totalRegistros = rs.getInt("contaRegistros");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.connection.close();
+		}
+		return totalRegistros;
+	}
+
+	public List<Bairro> getListaBairrosPaginada(int pagina, String ordenacao, String pesquisa, String campoPesquisa)
+			throws SQLException {
+		int limit = 10;
+		int offset = (limit * pagina) - limit;
+		String sql = "SELECT * FROM bairro WHERE " + campoPesquisa + " LIKE '%" + pesquisa + "%' ORDER BY " + ordenacao
+				+ " LIMIT 10 OFFSET " + offset;
+		List<Bairro> lista = new ArrayList<Bairro>();
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Bairro bairro = new Bairro();
+				bairro.setId(rs.getInt("id"));
+				bairro.setDescricao(rs.getString("descricao"));
+				lista.add(bairro);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.connection.close();
+		}
+		return lista;
+	}
+
+	public boolean excluirUsuario(Bairro bairro) throws SQLException {
+		String sql = "DELETE FROM bairro WHERE id = ?";
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setInt(1, bairro.getId());
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.connection.close();
+		}
+
+		return false;
+	}
+
+	public boolean alterarUsuario(Bairro bairro) throws SQLException {
+		String sql = "UPDATE bairro SET descricao=? WHERE id = ?";
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, bairro.getDescricao());
+			stmt.setInt(2, bairro.getId());
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.connection.close();
+		}
+
+		return false;
+	}
+
+	public boolean novoUsuario(Bairro bairro) throws SQLException {
+		String sql = "INSERT INTO bairro (descricao) VALUES (?)";
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, bairro.getDescricao());
+			stmt.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.connection.close();
+		}
+
+		return false;
+	}
+}
